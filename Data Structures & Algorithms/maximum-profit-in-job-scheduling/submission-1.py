d926c438625526b8d9b8b2b1dd3d9d23d8ab4d63
bishal <bishal0922@gmail.com>
2026-08-17 22:17:23 -0700

    Add: maximum-profit-in-job-scheduling - submission-1

---
+++ Data Structures & Algorithms/maximum-profit-in-job-scheduling/submission-1.py
@@ -0,0 +1,29 @@
+import bisect
+
+class Solution:
+    def jobScheduling(self, startTime: List[int], endTime: List[int], profit: List[int]) -> int:
+        jobs = sorted(zip(startTime, endTime, profit))
+        cache = {}
+
+        def dfs(i):
+            if i == len(jobs):
+                return 0
+            if i in cache: return cache[i]
+
+            res = dfs(i+1)
+
+            j = 0
+            while j < len(jobs):
+                if jobs[i][1] <= jobs[j][0]:
+                    break
+                j+=1
+
+            j = bisect.bisect_left(jobs, (jobs[i][1], -1, -1))
+
+            cache[i] = res = max(res, jobs[i][2] + dfs(j))
+            return res
+
+        return dfs(0)
+            
+
+        
